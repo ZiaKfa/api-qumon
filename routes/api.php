66cab98143aba366a87877592e72400e95e6ac50
ZiaKfa <zia14148@gmail.com>
2024-12-12 12:00:54 +0700

    implementasi basicauth

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/users', App\Http\Controllers\UserController::class);
-Route::apiResource('/quiz', App\Http\Controllers\QuizController::class);
-Route::apiResource('/kategori', App\Http\Controllers\CategoryController::class);
 Route::post('/register', 'App\Http\Controllers\AuthController@register');
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
-Route::get('/leaderboard', 'App\Http\Controllers\LeaderboardController@index');
-Route::get('/leaderboard/weekly', 'App\Http\Controllers\LeaderboardController@weekly');
+
+Route::middleware('auth.basic')->group(function () {
+    Route::apiResource('/users', App\Http\Controllers\UserController::class);
+    Route::apiResource('/quiz', App\Http\Controllers\QuizController::class);
+    Route::apiResource('/kategori', App\Http\Controllers\CategoryController::class);
+    Route::get('/leaderboard', 'App\Http\Controllers\LeaderboardController@index');
+    Route::get('/leaderboard/weekly', 'App\Http\Controllers\LeaderboardController@weekly');
+});
